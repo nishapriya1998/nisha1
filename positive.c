04e1a096eed26035f4b9705781eb8136d45ef0ef
nishapriya1998 <46838500+nishapriya1998@users.noreply.github.com>
2019-01-19 15:01:11 +0530

    positive.c

---
+++ positive.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int a,t;
+    scanf("%d",&t);
+    while(t>0)
+    {
+        scanf("%d",&a);
+        if(a>0)
+        printf("Positive");
+        else if(a<0)
+        printf("Negative");
+        else
+        printf("Zero");
+    }
+    t--;
+}
